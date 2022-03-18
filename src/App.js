8fd56e2ff19400778eaff30ed6c4dbb5a8bf6086
Kalu Chinonso <info@blacklistng.com>
2022-03-18 17:29:36 +0100

    removed routes - Active and Completed

--- src/App.js
+++ src/App.js
@@ -4,16 +4,12 @@ import './App.css';
 import * as ReactDOM from "react-dom";
 import Landing from './Pages/Landing/Landing';
 import { Routes, Route } from "react-router-dom";
-import Completed from './Pages/Completed/Completed';
-import Active from './Pages/Active/Active';
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Landing />} />
-        <Route path="completed" element={<Completed/>} />
-        <Route path="active" element={<Active/>} />
       </Routes>
     </>
   );
